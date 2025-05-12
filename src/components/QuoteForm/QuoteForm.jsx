865751ffaf61f071ee3814bb6da36c8fbfe64925
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2025-05-12 22:32:45 +0300

    feat: add ProgressBar component to QuoteForm

--- src/components/QuoteForm/QuoteForm.jsx
+++ src/components/QuoteForm/QuoteForm.jsx
@@ -1,9 +1,11 @@
+import ProgressBar from "./ProgressBar";
 import { QuoteFormProvider } from "./QuoteFormProvider";
 import QuoteFormSteps from "./QuoteFormSteps";
 
 function QuoteForm() {
   return (
     <QuoteFormProvider>
+      <ProgressBar />
       <QuoteFormSteps />
     </QuoteFormProvider>
   );
